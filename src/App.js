bf3b933230241ece797e9d31551f0f414170e309
andarpratama <master.amarta@gmail.com>
2021-06-26 09:56:32 +0700

    add feature add, delete todo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
-  return (
-    <div className="App">
+   const [inputText, setInputText] = useState("")
+   const [todos, setTodos] = useState([])
+
+   return (
+      <div className="App">
       <header>
          <h1>Andar Todo List</h1>
       </header>
-      <Form />
-    </div>
-  );
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList setTodos={setTodos} todos={todos} />
+      </div>
+   );
 }
 
 export default App;
